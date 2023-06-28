a3e7ec5d44e489119381ac12aeedaea7ad084e4c
warisu <abdullahiabdulwaris@gmail.comgit config --global user.email abdullahiabdulwaris@gmail.com>
2023-06-28 18:11:20 +0100

    reconfigure the code

--- src/info/info.js
+++ src/info/info.js
@@ -1,34 +1,21 @@
 import React from "react";
 import './info.css'
 
-const  Info = ()=> {
+const Info = ()=> {
     return (
     <div className="info">
-       <div>
         <h3>Personal Information</h3>
         <form>
-            <input type='text' id='taskInput' placeholder="Enter your name"/>
-            <input type='text' id='taskInput' placeholder="Enter your email"/>
-            <input type='text' id='taskInput' placeholder="Enter your phone number"/>
-            <input type="photo" id='taskInput' placeholder="Photo"/>
+            <input type='text' placeholder="First name"/>
+            <input type='text' placeholder="Last name"/>
+            <input type='text' placeholder="Title"/>
+            <input type="photo" placeholder="Photo"/>
+            <input type="text" placeholder="Address"/>
+            <input type="number" placeholder="Phone number"/>
+            <input type="email" placeholder="Email"/>
+            <input type="text" placeholder="Description"/>
         </form>   
-       </div>
-       <div>
-        <h3>Educational Experience</h3>
-        <form>
-            <input type='text' id='taskInput' placeholder="Title of study"/>
-            <input type='number' id='taskInput' placeholder="Date of study"/>
-            <imput type='number' id='taskInput' placeholder='Date of Graduation'/>
-        </form>
-       </div>
-       <div>
-        <h3>Practical Experience</h3>
-            <form>
-                <input type='text' id='taskInput' placeholder="How many years of programming language do you have"/>
-                <input type='number' id='taskInput' placeholder="How long have you use javascript?? "/>
-            </form>
-       </div>
-       </div>
+    </div>
     )
 }
 
